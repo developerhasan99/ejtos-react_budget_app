037adfafda6056dc749fd05a7d719e50fdf80b80
Mehedi Hasan <developerhasan99@gmail.com>
2024-03-04 23:27:40 -0500

    Completed practice

--- src/App.js
+++ src/App.js
@@ -1,43 +1,32 @@
 import React from 'react';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { AppProvider } from './context/AppContext';
 
 //Code to import Budget.js
 import Budget from './components/Budget';
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
+import ExpenseList from './components/ExpenseList';
+import AllocationForm from './components/AllocationForm';
 
-// Add code to import the other components here under
-
-
-import { AppProvider } from './context/AppContext';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
-                            /* Add Budget component here */
-                        }        
-
-                        {
-                            /* Add Remaining component here*/
-                        }        
-
-                        {
-                            /* Add ExpenseTotal component here */
-                        }        
+                        <div className='col-sm'>
+                            <Budget />   
+                        </div>
+                        <div className='col-sm'>
+                            <Remaining />  
+                        </div>
+                        <div className='col-sm'>
+                            <ExpenseTotal />  
+                        </div>
                        
-                        {
-                            /* Add ExpenseList component here */
-                        }         
-
-                        {
-                            /* Add ExpenseItem component here */
-                        }        
-
-                        {
-                            /* Add AllocationForm component here under */
-                        }        
+                        <ExpenseList />  
+                        <AllocationForm />
 
                 </div>
             </div>
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,1 +1,18 @@
+import React, {useContext} from 'react'
+import { AppContext } from '../context/AppContext';
 
+const Remaining = () => {
+    const {expenses, budget} = useContext(AppContext);
+    const totlaExpenses = expenses.reduce((total, item) => {
+        return (total = total + item.cost)
+    }, 0);
+
+    const alertType = totlaExpenses > budget ? 'alert-danger' : 'alert-success';
+    return (
+        <div className={`alert ${alertType}`}>
+            <span>Remaining: ${budget - totlaExpenses}</span>
+        </div>
+    );
+}
+
+export default Remaining;
